a9f78adbf6549f6d8b5b4bce4aea4a38b8c0d764
Valery Maksimovich <thuja@tut.by>
2023-02-26 23:01:02 +0300

    home work 12

---
+++ Lesson-12/build/type.js
@@ -0,0 +1,104 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = exports.filter = exports.map = void 0;
+const users = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep',
+        car: 'VW'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut',
+        children: 2
+    }
+];
+const persons = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Jane Doe',
+        age: 32,
+        role: 'Administrator'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    },
+    {
+        name: 'Bruce Willis',
+        age: 64,
+        role: 'World saver'
+    }
+];
+class ObjectManipulator {
+    constructor(obj) {
+        this.obj = obj;
+    }
+    set(key, value) {
+        return new ObjectManipulator(Object.assign(Object.assign({}, this.obj), { [key]: value }));
+    }
+    get(key) {
+        return this.obj[key];
+    }
+    delete(key) {
+        const newObj = Object.assign({}, this.obj);
+        delete newObj[key];
+        return new ObjectManipulator(newObj);
+    }
+    getObject() {
+        return this.obj;
+    }
+}
+// 4. Обеспечьте правильную типизацию для указанных функций.
+function map(mapper, input) {
+    if (arguments.length === 0) {
+        return map;
+    }
+    if (arguments.length === 1) {
+        return function subFunction(subInput) {
+            if (arguments.length === 0) {
+                return subFunction;
+            }
+            return subInput.map(mapper);
+        };
+    }
+    return input.map(mapper);
+}
+exports.map = map;
+function filter(filterer, input) {
+    if (arguments.length === 0) {
+        return filter;
+    }
+    if (arguments.length === 1) {
+        return function subFunction(subInput) {
+            if (arguments.length === 0) {
+                return subFunction;
+            }
+            return subInput.filter(filterer);
+        };
+    }
+    return input.filter(filterer);
+}
+exports.filter = filter;
+function add(a, b) {
+    if (arguments.length === 0) {
+        return add;
+    }
+    if (arguments.length === 1) {
+        return function subFunction(subB) {
+            if (arguments.length === 0) {
+                return subFunction;
+            }
+            return a + subB;
+        };
+    }
+    return a + b;
+}
+exports.add = add;
